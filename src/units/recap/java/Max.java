fdd76809f711837361a7614d940d6fa582637867
Alberto Cortés Martín <alcortes@it.uc3m.es>
2015-01-11 11:19:39 +0100

    Adds recap - the world acording to Java

---
+++ src/units/recap/java/Max.java
@@ -0,0 +1,19 @@
+// error checking skipped for didactic purposes
+class Max {
+    public static void main(String[] args) {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+
+        int c = max(a, b);
+
+        System.out.println(c);
+    }
+
+    static int max(int x, int y) {
+        if (x >= y) {
+            return x;
+        } else {
+            return y;
+        }
+    }
+}
