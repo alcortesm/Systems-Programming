2efcdcf10ceac0a4a28d9fc3d659c9353c44576c
Alberto Cortés Martín <alcortes@it.uc3m.es>
2015-01-21 20:45:00 +0100

    add info to music script

--- scripts/music-s01.sh
+++ scripts/music-s01.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-mplayer ~/room/personal/music/Apocalyptica/2010-7th_Symphony/09-Sacra.mp3
+echo "Apocalyptica - Sacra"
+echo "https://www.youtube.com/watch?v=wJumvqm0qqo"
+mplayer ~/room/personal/music/Apocalyptica/2010-7th_Symphony/09-Sacra.mp3 >/dev/null 2>&1
