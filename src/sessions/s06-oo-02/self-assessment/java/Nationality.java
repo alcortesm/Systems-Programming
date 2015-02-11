cc59aca06aaff350940bbfbba5a64905fc2ad0d6
Alberto Cortés Martín <alcortes@it.uc3m.es>
2015-02-11 16:27:56 +0100

    add child exercise

--- src/sessions/s06-oo-02/self-assessment/java/Nationality.java
+++ src/sessions/s06-oo-02/self-assessment/java/Nationality.java
@@ -6,7 +6,7 @@ final class Nationality {
     public final static Nationality ENGLISH = new Nationality("English");
     public final static Nationality NORTH_AMERICAN = new Nationality("North American");
     public final static Nationality ITALIAN = new Nationality("Italian");
-    public final static Nationality FRENCH = new Nationality("French");
+    public final static Nationality DUNEDAIN = new Nationality("Dúnedain");
     public final static Nationality UNKNOWN = new Nationality("unknown");
     public final static Nationality UNSUPPORTED = new Nationality("unsupported");
     public final static Nationality NO_NATIONALITY = new Nationality("no nationality");
